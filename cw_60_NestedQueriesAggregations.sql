8e7810bab1daaaa2dde6b3811217df1e971d94e1
OljaSmirnova <118764036+OljaSmirnova@users.noreply.github.com>
2024-01-08 20:00:27 +0200

    CW#60 & HW#60 Nested Queries Aggregations

---
+++ cw_60_NestedQueriesAggregations.sql
@@ -0,0 +1,19 @@
+ --get name of car owners having cars model1
+ --select distinct owner_id, (select name from car_owners where id = owner_id) from cars where model_name = 'model1'
+ --get full data about persons having no cars
+ --select * from car_owners where id not in(select owner_id from cars)
+ --select * from cars where number not in (select car_number from trade_deals)
+ --select * from car_owners where not exists(select * from cars where owner_id = id)
+ --
+ --Aggregations
+ --select round(avg(date_part('year', age(birth_date)))) as "average age" from car_owners
+ --select count(*) from cars where color='red'
+ --select color, count(*) from cars group by color
+ --select color, count(*), model_name from cars group by color, model_name
+    --get one most popular color
+  --select color, count(*) as amount from cars group by color order by amount desc limit 1
+  --select color, count(*) as amount from cars group by color having count(*) > 3
+    --get all most popular colors
+  --select color, count(*) as amount from cars group by color having count(*) = 
+  --(select max(count) from (select count(*) as count from cars group by color) counts)
+ 
