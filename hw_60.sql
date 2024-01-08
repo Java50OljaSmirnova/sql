0e02dda5894124f87bf4bf42696a7dbacf21fb48
OljaSmirnova <118764036+OljaSmirnova@users.noreply.github.com>
2024-01-08 21:37:21 +0200

    HW#60

--- hw_60.sql
+++ hw_60.sql
@@ -1,18 +1,13 @@
  --HW#60
- --Task #1
  --select distinct model_name from cars where owner_id in (select id from car_owners where extract (year from age(birth_date))  > 
-    --(select round(avg(date_part('year', age(birth_date)))) as "average age" from car_owners) * 1.1) 
+ --(select round(avg(date_part('year', age(birth_date)))) as "average age" from car_owners) * 1.1) 
  --
- --Task #2
  --select name from models where engine_capacity = (select max(engine_capacity) from models);
  --
- --Task #3
  --select model_name, count(*) as amount from cars group by model_name order by amount desc FETCH FIRST 2 ROWS ONLY;
  --
- --Task #4
  --select model_name, count(color) as amount from cars group by model_name having count(*) >= 2 order by amount asc
  --
- --Task #5
  --select (floor(extract (year from age(birth_date))/10) * 10 ) as min, 
  --(floor(extract (year from age(birth_date))/10) * 10 + 10) as max,  count(*) as frequency from car_owners
  -- group by min, max order by min
